310e04d325680f78e18a3c18a354f3112383f4e0
George Rozitis <namacker.fire@gmail.com>
2024-12-08 02:40:05 +0100

    Added function for contestant user input

--- survivorsimulator.js
+++ survivorsimulator.js
@@ -15,7 +15,7 @@ function generateUserTable() {
 
     for (row of userRows) {
         userTable.insertAdjacentHTML('beforeend',
-            '<tr><td>' + row + ': </td><td><input type="text" id="userTable' + row + '"></td></tr>');
+            `<tr><td>${row}: </td><td><input type="text" id="userTable${row}"></td></tr>`);
         console.log(row);
     }
 }
@@ -25,20 +25,46 @@ function generateUserTable() {
  - Contestants is a positive integer >= 3.
  - Days is a positive integer.
  - Tribes is a positive integer.
+ @return A boolean - true if the above conditions hold, false otherwise.
  */
 function checkUserConfig() {
     const userRows = getUserVariables();
     
     for (row of userRows) {
-        var rowValue = document.getElementById("userTable" + row).value;
-        console.log(row + ": " + rowValue);
+        var rowValue = document.getElementById(`userTable${row}`).value;
+        console.log(`${row}: ${rowValue}`);
+
         if (isNaN(rowValue) || parseInt(Number(rowValue)) != rowValue || isNaN(parseInt(rowValue, 10))) {
-            console.log(row + " must be an integer!");
+            console.log(`${row} must be an integer!`);
+            return false;
         } else {
             var rowValueParsed = parseInt(Number(rowValue));
-            if (row == "Contestants" && rowValueParsed < 3) console.log("Contestants must be >= 3!")
-            if (row == "Days" && rowValueParsed < 1) console.log("Days must be >= 1!");
-            if (row == "Tribes" && rowValueParsed < 1) console.log("Tribes must be >= 1!");
+
+            if (row == "Contestants" && rowValueParsed < 3) {
+                console.log("Contestants must be >= 3!");
+                return false;
+            } else if (row == "Days" && rowValueParsed < 1) {
+                console.log("Days must be >= 1!");
+                return false;
+            } else if (row == "Tribes" && rowValueParsed < 1) {
+                console.log("Tribes must be >= 1!");
+                return false;
+            }
         }
     }
+    generateContestantsInput(parseInt(Number(document.getElementById("userTableContestants").value)));
+    return true;
+}
+
+function generateContestantsInput(numContestants) {
+    // const contestantsDiv = document.getElementById("contestantsConfig");
+    const contestantsTable = document.createElement("table");
+    for (i = 0; i < numContestants; i++) {
+        var row = contestantsTable.insertRow();
+        var cellName = row.insertCell(0);
+        cellName.innerHTML = `Contestant # ${(i + 1)}`;
+        var cellNameInput = row.insertCell(1);
+        cellNameInput.innerHTML = `Name: <input type="text">`;
+    }
+    document.getElementById("contestantsConfig").appendChild(contestantsTable);
 }
