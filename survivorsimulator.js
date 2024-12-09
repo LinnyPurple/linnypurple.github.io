8ccc0afe3950cc3458d1c6cb729bb227eddce586
George Rozitis <namacker.fire@gmail.com>
2024-12-09 23:59:37 +0100

    Added functionality for tribe input

--- survivorsimulator.js
+++ survivorsimulator.js
@@ -34,9 +34,11 @@ function checkUserConfig() {
         var rowValue = document.getElementById(`userTable${row}`).value;
         console.log(`${row}: ${rowValue}`);
 
+        // Checks if rowValue is an integer
         if (isNaN(rowValue) || parseInt(Number(rowValue)) != rowValue || isNaN(parseInt(rowValue, 10))) {
             console.log(`${row} must be an integer!`);
             return false;
+        // Checks if rowValue fits within the parameters for each user variable
         } else {
             var rowValueParsed = parseInt(Number(rowValue));
 
@@ -53,25 +55,26 @@ function checkUserConfig() {
         }
     }
     generateContestantsInput(parseInt(Number(document.getElementById("userTableContestants").value)));
+    generateTribesInput(parseInt(Number(document.getElementById("userTableTribes").value)));
     return true;
 }
 
 function generateContestantsInput(numContestants) {
-    // const contestantsDiv = document.getElementById("contestantsConfig");
+    document.getElementById("contestantsConfig").innerHTML += "<h3>Contestants Input</h3>";
     const contestantsTable = document.createElement("table");
     const genders = ["Female", "Male", "None"];
     for (i = 0; i < numContestants; i++) {
         var row = contestantsTable.insertRow();
 
-        // Column 0: Says "Contestant #(i+1)" (C_i)
+        // Column 0: Says "Contestant #(i+1)" (C_k)
         var cellName = row.insertCell(0);
         cellName.innerHTML = `Contestant #${(i + 1)}`;
 
-        // Column 1: Name input for C_i
+        // Column 1: Name input for C_k
         var cellNameInput = row.insertCell(1);
         cellNameInput.innerHTML = `Name: <input type="text">`;
         
-        // Column 2: Gender input for C_i
+        // Column 2: Gender input for C_k
         var cellGenderInput = row.insertCell(2);
         var cellGenderHTML = `<select>`;
         for (gender of genders) {
@@ -80,4 +83,29 @@ function generateContestantsInput(numContestants) {
         cellGenderInput.innerHTML = `Gender: ${cellGenderHTML}</select>`;
     }
     document.getElementById("contestantsConfig").appendChild(contestantsTable);
+}
+
+function generateTribesInput(numTribes) {
+    document.getElementById("contestantsConfig").innerHTML += "<h3>Tribes Input</h3>"
+    const tribesTable = document.createElement("table");
+    for (i = 0; i < numTribes; i++) {
+        var row = tribesTable.insertRow();
+
+        // Column 0: Says "Tribe #(i+1)" (T_k)
+        var cellName = row.insertCell(0);
+        cellName.innerHTML = `Tribe #${i + 1}`;
+
+        // Column 1: Name input for T_k
+        var cellNameInput = row.insertCell(1);
+        cellNameInput.innerHTML = `Name: <input type="name">`;
+
+        // Column 2: Initial tribe members input for T_k
+        var cellInitialMembersInput = row.insertCell(2);
+        cellInitialMembersInput.innerHTML = `Initial Members: <input type="name">`;
+
+        // Column 3: Tribe color input for T_k
+        var cellTribeColorInput = row.insertCell(3);
+        cellTribeColorInput.innerHTML = `Tribe Color: <input type="color">`;
+    }
+    document.getElementById("tribesConfig").appendChild(tribesTable);
 }
